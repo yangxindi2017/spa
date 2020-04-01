91c6b3ff590e1d5122187a88cc462ad3cc11e5a4
yangxindi2017 <694076236@qq.com>
2020-04-01 10:58:08 +0800

    add router & store

---
+++ 51-fe-router/main.js
@@ -0,0 +1,11 @@
+$(function(){
+  window.onhashchange = function(){
+    var $block = $('#main'),
+        strHash =window.location.hash,
+        color = strHash.substring(3,strHash.length);
+  
+    $block.css({
+      'background-color':color
+    })
+  }
+})
